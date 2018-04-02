f835de08ad1ecb368a717560afdff058dc31af20
Li-Wei Cheng <z789512346@gmail.com>
2018-04-02 10:23:54 +0800

    implement parse quest section

--- dns.c
+++ dns.c
@@ -42,38 +42,97 @@ static struct dns_header *dns_header_alloc(struct pkt_proc *pp)
 	dh->ns_count = ntohs(h->ns_count);
 	dh->ar_count = ntohs(h->ar_count);
 
+	DBG("\nparse header:\n"
+		"\tid(0x%x)\n"
+		"\tflag_code(0x%x)\n"
+		"\tqd_count(%d)\n"
+		"\tan_count(%d)\n"
+		"\tns_count(%d)\n"
+		"\tar_count(%d)\n",
+		dh->id, dh->flag_code, dh->qd_count,
+		dh->an_count, dh->ns_count, dh->ar_count);
+
 	return dh;
 }
 static int parse_domain_name(struct pkt_proc *pp, struct dns_quest *dq)
 {
-	//TODO
-	return -1;
+	const u_char *p = pp->pkt + pp->offset;
+	const u_char *tail = pp->pkt + pp->len;
+
+	int total_len = 0;
+	int label_len = 0;
+	int in_ptr = 0;
+	memset(dq, 0, sizeof(struct dns_quest));
+
+	while (p < tail && *p && total_len < MAX_DOMAIN_LEN) {
+		if (!in_ptr)
+			pp->offset++;
+		if (*p == 0xc0) {
+			//ptr
+			int offset = (*p) & 0x3F;
+			p++;
+			if (p >= tail || *p)
+				break;
+			offset = offset * 16 + *p;
+			if (offset >= pp->len)
+				break;
+			p = pp->pkt + offset;
+			label_len = 0;
+			in_ptr = 1;
+		}
+		else if (label_len) {
+			dq->qname[total_len++] = *p;
+			p++;
+			label_len--;
+		}
+		else {
+			dq->qname[total_len++] = '.';
+			label_len = *p;
+			p++;
+		}
+	}
+	if (*p || p == tail)
+		return -1;
+	dq->qname[total_len] = 0;
+	DBG("parsed domain name: %s\n", dq->qname);
+	return total_len;
 }
-static struct dns_quest *dns_query_alloc(const struct dns_header *hdr,
-					struct pkt_proc *pp)
+static int parse_quest_section(struct pkt_proc *pp,
+				int qd_count, struct dns_quest *dq)
 {
 	int i;
-	struct dns_quest *dq = calloc(1, sizeof(struct dns_quest) * hdr->qd_count);
-	if (!dq) {
-		ERR("Cannot allocate for dns quest\n");
-		return NULL;
-	}
-	for (i = 0; i < hdr->qd_count; i++) {
-		//parse name first
-		if (!parse_domain_name(pp, &dq[i])) {
+	for (i = 0; i < qd_count; i++) {
+		//parse name
+		if (parse_domain_name(pp, &dq[i]) < 0) {
 			ERR("parse_domain_name %d failed\n", i);
-			goto err;
+			return -1;
 		}
 
+		//parse type and class
 		if (pp->offset + sizeof(uint16_t) * 2 > pp->len) {
 			ERR("Invalid pkt\n");
-			goto err;
+			return -1;
 		}
 		dq[i].qtype = ntohs(*((uint16_t *)pp->pkt + pp->offset));
 		pp->offset += sizeof(uint16_t);
 
 		dq[i].qclass = ntohs(*((uint16_t *)pp->pkt + pp->offset));
 		pp->offset += sizeof(uint16_t);
+	}
+	return 0;
+}
+static struct dns_quest *dns_query_alloc(const struct dns_header *hdr,
+					struct pkt_proc *pp)
+{
+	struct dns_quest *dq = calloc(1, sizeof(struct dns_quest) * hdr->qd_count);
+	if (!dq) {
+		ERR("Cannot allocate for dns quest\n");
+		return NULL;
+	}
+
+	if (parse_quest_section(pp, hdr->qd_count, dq)) {
+		ERR("parse_quest_section failed\n");
+		goto err;
 	}
 
 	return dq;
@@ -115,8 +174,6 @@ struct dns_pkt *dns_alloc(const u_char *pkt, unsigned int len)
 	}
 
 	qr_code = dns_qr(dp->hdr);
-	if (qr_code == DNS_QR_QUERY) {
-	}
 	switch (qr_code) {
 	case DNS_QR_QUERY:
 		dp->quests = dns_query_alloc(dp->hdr, &pp);
