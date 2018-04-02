cdd8e946f34ba0189130eb2f9d43e76cece67abc
Li-Wei Cheng <z789512346@gmail.com>
2018-04-02 14:30:34 +0800

    add debug control def

--- common.h
+++ common.h
@@ -4,7 +4,16 @@
 #include <stdio.h>
 
 #define ERR(fmt, ...) fprintf(stderr, "*** ERR [%s-%d]: "fmt, __func__, __LINE__, ##__VA_ARGS__)
+
+#ifdef _DNS_PARSER_DBG_
+
 #define DBG(fmt, ...) fprintf(stdout, "[%s-%d]: "fmt, __func__, __LINE__, ##__VA_ARGS__)
+
+#else
+
+#define DBG
+
+#endif
 
 #endif
 
